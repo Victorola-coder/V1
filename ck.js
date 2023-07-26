da4e959132889de3989f5e9af50c346d384bdcdf
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-07-27 00:29:33 +0100

    Final Changes

--- ck.js
+++ ck.js
@@ -31,7 +31,7 @@ export default {
           const cookie = cookies[i];
           const eqPos = cookie.indexOf('=');
           const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-          document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=${Q.location.hostname};path=/`;
+          document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=${location.hostname};path=/`;
         }
     }      
 };
