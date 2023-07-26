6874f0909871e015a82edbf1f3b1e65a4ae515e4
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-07-26 08:18:13 +0100

    Functional login and signup

--- api.js
+++ api.js
@@ -1,26 +1,36 @@
 import ck from "./ck.js"
 /* Communicate with firebase */
 const config = { apiKey: "AIzaSyCSvEKkCHzclAOZY8xJB12eqEFcEexfHYg", authDomain: "vickyjay.firebaseapp.com", databaseURL: "https://vickyjay-default-rtdb.firebaseio.com", projectId: "vickyjay", storageBucket: "vickyjay.appspot.com", messagingSenderId: "587289416599", appId: "1:587289416599:web:39febd588afe384c36aad2", measurementId: "G-5YZJHG9TNZ" };
-const app = (await import('https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js')).initializeApp(config) // initialize app
+const app = (await import('https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js')).initializeApp(config) //initialize app
 const analytics = (await import('https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js')).getAnalytics(app); //analytics
 const storage = await import('https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js');
 const database = await import('https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js');
 const db = database.getDatabase(app); //Firebase Database reference
-const storageRef = database.getStorage(app); //Firebase Storage Reference;
+const storageRef = storage.getStorage(app); //Firebase Storage Reference;
 
 /* Create Account */
-const signbtn = document.getElementById("sg-btn") //Sign up button;
-const email = document.getElementById("email").value //Email from text input
+const ft = (x) => document.getElementById(x) || document.createElement("div");
+const signbtn = ft("sgn") //Sign up button;
+const err = ft("err");
+const form = ft("form");
 const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 signbtn.onclick = async() => {
+    console.log("Y");
     //validate form
-    if(!regex.test(email)){
-        //indicate error
+    if(!regex.test(form.email.value)){
+        err.innerText = "Invalid Email";
+        form.email.onclick = () => err.innerText = '';
         return;
-    
     }
     //generate userid
-    const uid = Object.keys((await database.get(database.ref(db, '/uid/'))).val()) //get existing ID's from database
+    const xl = (await database.get(database.ref(db, '/uid/'))).val() //get existing ID's from database
+    const xm = Object.values(xl).map(a => a.email);
+    if(xm.some(a => a.toLowerCase() == form.email.value.toLowerCase())){
+        err.innerHTML = "Email already exists <br> <a style=\"color:white\" href=\"login.html\">Sign In?</a>"
+        form.email.onclick = () => err.innerHTML = '';
+        return;
+    }
+    const uid = Object.keys(xl);
     function generateId() {
         let id = '';
         let char = Array(10).fill(0).map((a,b) => a+b);
@@ -34,41 +44,46 @@ signbtn.onclick = async() => {
     let userId = generateId();
     while(uid.includes(userId)){userId = generateId()};
     //create user
-    await database.set(database.ref(db, `/uid/${userId}`), ({userId, email}));
+    await database.set(database.ref(db, `/uid/${userId}`), ({userId, email: form.email.value, pwd: form.pwd.value}));
     ck.set("uid", userId);
     //return to main page
     location.href = '/';
 }
 
 /* Login */
-const loginbtn = document.getElementById("lg-btn") //Login button
+const loginbtn = ft("lgn");
 loginbtn.onclick = async() => {
-    if(!regex.test(email)){
-        //indicate error
+    if(!regex.test(form.email.value)){
+        err.innerText = "Invalid Email";
+        form.email.onclick = () => err.innerText = '';
         return;
     }
     const uid = (await database.get(database.ref(db, '/uid/'))).val();
+    console.log(uid);
     let userId;
-    for(const id of uid){
+    for(const id in uid){
         const data = uid[id];
-        if(data.email.toLowercase() == email.toLowercase()){
+        if(data.email.toLowerCase() == form.email.value.toLowerCase() && data.pwd.toLowerCase() == form.pwd.value.toLowerCase()){
             userId = data.userId;
             break;
         }
     }
+    console.log(Boolean())
     if(userId){
         //return to homepage
         ck.set("uid", userId);
         location.href = '/';
     } else {
-        //warn user
+        err.innerText = "Account not found";
+        form.email.onclick = () => err.innerText = '';
+        form.pwd.onclick = () => err.innerText = '';
         return;
     }
 }
 
 /* Upload files */
-const uploadbtn = document.getElementById("upload"); //upload button;
-const description = document.getElementById("description").value //Description or links if any
+const uploadbtn = ft("upload"); //upload button;
+const description = ft("description").value //Description or links if any
 uploadbtn.onclick = async() => {
     const uploads = Array.from(document.querySelectorAll("[type=file")).map(a => a.files) || Array.from(document.getElementById("file").files) //extract all uploads
     const day = document.getElementById("day").value;
